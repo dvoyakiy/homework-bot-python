b5e026a5dfd8c44b0cd054b3b668a24051275687
dvoyakiy <dvoyakiyyy@gmail.com>
2020-02-15 16:49:19 +0200

    add route for chat list

---
+++ api/routes/chats.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const {authRequired} = require('../middleware/authRequired');
+
+
+router.route('/')
+    .post(authRequired, (req, res) => {
+        res.json(req.user);
+    });
+
+module.exports = router;
+
